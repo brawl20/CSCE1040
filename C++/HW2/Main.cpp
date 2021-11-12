27c302588a94e5bac58966481cab77efb828ae7b
MichealSTEMuli <micheal@stemuli.net>
2021-11-12 15:24:57 -0600

    Staging changes beofre I test makefile

--- C++/HW2/Main.cpp
+++ C++/HW2/Main.cpp
@@ -17,6 +17,10 @@ void Menu()
     //Variables
     int userChoice;
     char userName[40];
+    ofstream fout;
+    ifstream fin;
+    string tempHolder;
+    bool inSystem = false;
 
     // Body
     cout << "*********Welcome to GreenBox!*********" << endl;
@@ -35,17 +39,40 @@ void Menu()
             cout << "Please enter your name:\n";
             cin.ignore();
             cin.getline(userName, 40);
+            fin.open("GreenBox-Data.txt");
+            if (fin.fail())
+            {
+                fout.open("GreenBox-Data.txt", ios_base::app);
+                fin.open("GreenBox-Data.txt");
+            }
+            
+            while(!fin.eof())
+            {
+                getline(fin, tempHolder);
+                if (tempHolder == userName)
+                {
+                    inSystem = true;
+                    cout << "This customer is already in our system.\n";
+                    break;
+                }
+            }
+            if (inSystem == false)
+            {
+                fout << "Customer Name:\n" << userName << "\t";
+            }
             break;
         default:
             cout << "Sorry, the choice you made was invalid. Please try again.\n";
             break;
         }
+        cout << "Menu:" << endl;
+        cout << "1) Returning Customer\n2) New Customer\n3) Exit\n";
         cin >> userChoice;
     }
 }
 
 int main()
-{
+{   
     Menu();
     return 0;
 }
