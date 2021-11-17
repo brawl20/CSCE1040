77f0ec041190b0e3239b6ae6a012af33bdddc1dd
MichealSTEMuli <micheal@stemuli.net>
2021-11-16 20:37:55 -0600

    Adding comment blocks to header

--- C++/HW2/CustomerCollection.cpp
+++ C++/HW2/CustomerCollection.cpp
@@ -1,3 +1,10 @@
+/*
+	Author:			Micheal Landsbaum (MichealLandsbaum@my.unt.edu)
+	Date:			11/16/2021
+	Course:         CSCE1040
+	Assignment:     Homework 3
+*/
+
 #include "Customer.h"
 #include "CustomerCollection.h"
 #include "Movie.h"
--- C++/HW2/Movie.h
+++ C++/HW2/Movie.h
@@ -1,6 +1,13 @@
 #ifndef MOVIE_H
 #define MOVIE_H
 
+/*
+	Author:			Micheal Landsbaum (MichealLandsbaum@my.unt.edu)
+	Date:			11/16/2021
+	Course:         CSCE1040
+	Assignment:     Homework 3
+*/
+
 #include <string>
 #include <iostream>
 
